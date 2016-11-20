3cee2027a65d95eba049a0bb0d1a3b610934d0a3
Cow-Fu <nathanj758@gmail.com>
2016-11-20 03:14:27 -0500

    QueryMasterServer class now functions properly

--- src/rocks/cow/QueryMasterServer/QueryMasterServer.java
+++ src/rocks/cow/QueryMasterServer/QueryMasterServer.java
@@ -1,14 +1,43 @@
-package rocks.cow.QueryMasterServer
+package rocks.cow.QueryMasterServer;
 
-import org.w3c.dom.Document
-import org.xml.sax.SAXException
-import rocks.cow.MasterServer.MasterServer
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import rocks.cow.MasterServer.MasterServer;
+import rocks.cow.Server.Server;
+import rocks.cow.Server.ServerBuilder;
 
-import javax.xml.parsers.ParserConfigurationException
-import java.io.IOException
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import java.io.IOException;
+import java.util.ArrayList;
 
-class QueryMasterServer {
-    val rawResult: Document
-        @Throws(IOException::class, SAXException::class, ParserConfigurationException::class)
-        get() = MasterServer.masterServerQuery()
+public class QueryMasterServer {
+    private XPath xpath;
+
+    public QueryMasterServer() {
+        xpath = XPathFactory.newInstance().newXPath();
+    }
+
+    public Document getRawResult() throws IOException, SAXException, ParserConfigurationException {
+        return MasterServer.masterServerQuery();
+    }
+
+    public ArrayList<Server> getServerList() throws ParserConfigurationException, SAXException, IOException, XPathExpressionException {
+        ServerBuilder serverBuilder = new ServerBuilder();
+        ArrayList<Server> servers = new ArrayList<Server>();
+        Document doc = getRawResult();
+
+        NodeList nodelist = (NodeList) xpath.compile("/qstat/server").evaluate(doc, XPathConstants.NODESET);
+
+        for (int i = 1; i < nodelist.getLength(); ++i) {
+            Node node = nodelist.item(i);
+            servers.add(serverBuilder.build(node));
+        }
+        return servers;
+    }
 }
