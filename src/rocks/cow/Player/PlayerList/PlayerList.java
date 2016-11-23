dafd5551a94b02aa9d95135a452d0a0088a564a5
Cow-Fu <nathanj758@gmail.com>
2016-11-23 14:57:53 -0500

    Added Query interface and PlayerList class

---
+++ src/rocks/cow/Player/PlayerList/PlayerList.java
@@ -0,0 +1,8 @@
+package rocks.cow.Player.PlayerList;
+
+import rocks.cow.Player.Player;
+
+import java.util.ArrayList;
+
+public class PlayerList extends ArrayList<Player> {
+}
---
+++ src/rocks/cow/Query/Query.java
@@ -0,0 +1,8 @@
+package rocks.cow.Query;
+
+import org.w3c.dom.Document;
+
+public interface Query {
+    Document query();
+
+}
