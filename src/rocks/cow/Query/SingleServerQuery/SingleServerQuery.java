cde5783752decae1e42507141cf1b69551d1e19f
Nathan Justinian <nathanj758@gmail.com>
2016-12-12 21:38:33 -0500

    Completed SingleServerQuery

--- src/rocks/cow/Query/SingleServerQuery/SingleServerQuery.java
+++ src/rocks/cow/Query/SingleServerQuery/SingleServerQuery.java
@@ -1,22 +1,35 @@
 package rocks.cow.Query.SingleServerQuery;
 
+
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 import rocks.cow.MasterServer.MasterServer;
 import rocks.cow.Query.Query;
+import rocks.cow.Server.Server;
+import rocks.cow.Server.ServerBuilder;
 
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class SingleServerQuery implements Query {
     public String targetServerIP = "";
+    private XPath xpath;
     private Logger logger = Logger.getGlobal();
 
-    public SingleServerQuery() {}
+    public SingleServerQuery() {
+        xpath = XPathFactory.newInstance().newXPath();
+    }
 
     public SingleServerQuery(String ip) {
+        this();
         setTargetServerIP(ip);
     }
 
@@ -43,5 +56,15 @@ public class SingleServerQuery implements Query {
             logger.log(Level.SEVERE, "Unable to read data from the master server!");
         }
         return doc;
+    }
+
+    public Server getServer() throws XPathExpressionException {
+        ServerBuilder serverBuilder = new ServerBuilder();
+        Document doc = query();
+
+        Node node = (Node) xpath.compile("/qstat/server")
+                .evaluate(doc, XPathConstants.NODE);
+
+        return serverBuilder.build(node);
     }
 }
