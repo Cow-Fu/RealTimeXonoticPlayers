d1d7a76b5253dd29aa1ed61507e893ea2579eb3d
Nathan Justinian <nathanj758@gmail.com>
2016-12-14 03:45:21 -0500

    Changed TableRenderer to a abstract class and added new functionality

--- src/rocks/cow/TableRenderer/TableRenderer.java
+++ src/rocks/cow/TableRenderer/TableRenderer.java
@@ -1,7 +1,19 @@
 package rocks.cow.TableRenderer;
 
 import de.vandermeer.asciitable.v2.RenderedTable;
+import de.vandermeer.asciitable.v2.V2_AsciiTable;
+import de.vandermeer.asciitable.v2.render.*;
+import de.vandermeer.asciitable.v2.themes.V2_E_TableThemes;
 
-public interface TableRenderer {
-    RenderedTable render();
+public abstract class TableRenderer {
+    protected V2_AsciiTable at = new V2_AsciiTable();
+    protected V2_AsciiTableRenderer rend = new V2_AsciiTableRenderer();
+
+    protected void initTable() {
+        rend.setTheme(V2_E_TableThemes.PLAIN_7BIT_STRONG.get());
+        rend.setWidth(new WidthLongestLine());
+    }
+
+    public abstract RenderedTable render();
+
 }
