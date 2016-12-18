55b6d1f61807013e64de6611a18d293cdde1f6a4
Nathan Justinian <nathanj758@gmail.com>
2016-12-18 16:22:41 -0500

    Added FileManager class

---
+++ src/rocks/cow/Util/FileManager.java
@@ -0,0 +1,61 @@
+package rocks.cow.Util;
+
+import java.io.*;
+
+public class FileManager {
+    private File file;
+
+    public FileManager() {}
+
+    public FileManager(String filePath) {
+        setFile(filePath);
+    }
+
+    public FileManager(File file) {
+        setFile(file);
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    public void setFile(String filePath) {
+        setFile(new File(filePath));
+    }
+
+    public boolean fileExists() {
+        return file.exists();
+    }
+
+    public boolean filePathExists() {
+        File parent = new File(file.getParent());
+        return parent.exists() && parent.isDirectory();
+    }
+
+    public boolean createPath() {
+        File parent = new File(file.getParent());
+        return parent.mkdirs();
+    }
+
+    public boolean createFile() throws IOException {
+        return createPath() && file.createNewFile();
+    }
+
+    public String read() throws IOException {
+        String temp = "";
+        String line;
+
+        BufferedReader fr = new BufferedReader(new FileReader(file));
+
+        while((line = fr.readLine()) != null) {
+            temp = temp.concat(line + "\n");
+        }
+        fr.close();
+        return temp;
+    }
+    public void write(String data) throws IOException {
+        BufferedWriter fw = new BufferedWriter(new FileWriter(file));
+        fw.write(data);
+        fw.close();
+    }
+}
