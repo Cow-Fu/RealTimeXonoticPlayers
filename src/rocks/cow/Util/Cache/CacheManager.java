437a17f0bf46858402f00e0b29a952b2e353a629
Nathan Justinian <nathanj758@gmail.com>
2017-01-24 04:15:53 -0500

    Began work on CacheManager

--- src/rocks/cow/Util/Cache/CacheManager.java
+++ src/rocks/cow/Util/Cache/CacheManager.java
@@ -1,5 +1,59 @@
 package rocks.cow.Util.Cache;
 
-public class CacheManager {
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.apache.commons.lang3.SystemUtils;
+import rocks.cow.Util.Cache.CachedServer.CachedServer;
+import rocks.cow.Util.Cache.CachedServer.CachedServerList.CachedServerList;
+import rocks.cow.Util.FileManager;
 
+import java.io.IOException;
+import java.lang.reflect.Type;
+
+public class CacheManager {
+    private FileManager fm = new FileManager();
+    private Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private String fp = System.getProperty("user.home");
+    private CachedServerList cachedServers;
+
+    public CacheManager() {
+        setOs();
+        fm.setFile(fp);
+    }
+
+    private void setOs() {
+        fp = System.getProperty("user.home");
+
+        if (SystemUtils.IS_OS_LINUX) {
+            fp += ".cache/XonoticServerCache.json";
+        } else if(SystemUtils.IS_OS_WINDOWS){
+            // fp += "";  //figure out a path
+        }
+    }
+
+    private CacheManager createCache() {
+        if (!fm.fileExists()) {
+            fm.createFile();
+        }
+        return this;
+    }
+
+    public void loadCache() {
+        String data = null;
+        try {
+            if ((data = fm.read()) != null) {
+                if (!data.equals("")) {
+                    Gson gson = new GsonBuilder().create();
+                    cachedServers = new CachedServerList();
+                    cachedServers.addAll(gson.fromJson(data, (Type) CachedServer[].class));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public CachedServerList getCachedServers() {
+        return cachedServers;
+    }
 }
